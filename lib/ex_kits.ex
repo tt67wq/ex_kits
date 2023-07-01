fc8fb390670841c58a0968d202fa86be77d02b31
tt67wq <tt67wq@aliyun.com>
2023-07-01 22:36:58 +0800

    feat: first commit

---
+++ lib/ex_kits.ex
@@ -0,0 +1,18 @@
+defmodule ExKits do
+  @moduledoc """
+  Documentation for `ExKits`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExKits.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
