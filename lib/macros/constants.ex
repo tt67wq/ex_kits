67f48af1798560fdcee72bb392d46dac7b6be6b5
wanqiang <wanqiang@shizhuang-inc.com>
2023-08-09 18:55:01 +0800

    feat: update constant impl

--- lib/macros/constants.ex
+++ lib/macros/constants.ex
@@ -6,11 +6,14 @@ defmodule ExKits.Macros.Constants do
     const :facebook_url, "http://facebook.com/rohanpujaris"
   end
 
-  MyApp.Constant.facebook_url  # You can use this line anywhere to get the facebook url.
+  defmodule MyApp do
+    require MyApp.Constant
+    @facebook_url MyApp.Constant.facebook_url  # You can use this line anywhere to get the facebook url.
+  end
   """
   defmacro const(const_name, const_value) do
     quote do
-      def unquote(const_name)(), do: unquote(const_value)
+      defmacro unquote(const_name)(), do: unquote(const_value)
     end
   end
 end
