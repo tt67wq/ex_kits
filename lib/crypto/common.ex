6249a2cb2728239151e2d9aff61d17a52530ea94
tt67wq <tt67wq@aliyun.com>
2023-09-21 22:42:26 +0800

    feat: add generate_id func

--- lib/crypto/common.ex
+++ lib/crypto/common.ex
@@ -32,4 +32,34 @@ defmodule ExKits.Crypto.Common do
     |> Base.url_encode64()
     |> binary_part(0, length)
   end
+
+
+  @doc """
+  generate short random id
+
+  ## Examples
+
+  iex> generate_id("H")
+  "H68203790HX446F"
+  """
+  @spec generate_id(String.t()) :: String.t()
+  def generate_id(prefix) do
+    mid =
+      1..8
+      |> Enum.map(fn _ -> Enum.random(0..9) end)
+      |> Enum.join()
+
+    "#{prefix}#{mid}#{gen_reference()}"
+  end
+
+  defp gen_reference() do
+    min = String.to_integer("100000", 36)
+    max = String.to_integer("ZZZZZZ", 36)
+
+    max
+    |> Kernel.-(min)
+    |> :rand.uniform()
+    |> Kernel.+(min)
+    |> Integer.to_string(36)
+  end
 end
