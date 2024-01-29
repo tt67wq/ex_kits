d59a33f8e65f62f208164df3a9078fa67282833f
wanqiang <wanqiang@shizhuang-inc.com>
2024-01-29 17:41:48 +0800

    Refactor exception details handling

--- lib/utils/exception.ex
+++ lib/utils/exception.ex
@@ -3,6 +3,10 @@ defmodule ExKits.Utils.Exception do
   General exception with an optional string, map, or Keyword list stored
   in exception details
   """
+  @type t :: %__MODULE__{
+          message: String.t() | nil,
+          details: any()
+        }
   defexception [:message, :details]
 
   def message(%__MODULE__{} = exception) do
@@ -14,7 +18,7 @@ defmodule ExKits.Utils.Exception do
     end
   end
 
-  defp details(e) when is_map(e), do: ": " <> (Map.to_list(e) |> inspect())
+  defp details(e) when is_map(e), do: ": " <> (e |> Map.to_list() |> inspect())
   defp details(e) when is_binary(e), do: ": " <> e
   defp details(nil), do: ""
   defp details(e), do: ": " <> inspect(e)
